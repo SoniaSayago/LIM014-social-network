1d0f84e4a09c7492984c5003ec393911745081a0
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-04-27 19:54:43 -0500

    avance registro

--- src/view/register.js
+++ src/view/register.js
@@ -1,10 +1,22 @@
 export default () => {
     const viewRegister = `
-    <h1> Registro </h1>
+   <div class= 'whiteBackground'>    
+   <h> Registro </h1>
+    <p>Crea tu cuenta con</p>
+    <div class= imagesContainer></div>
+    <p>ó registrate fácilmente con tus datos:</p>
     <div class= 'formContainer'>
-    <div class ='formDiv'><form id='registerForm'>
-        <input type='text' id='name' placeholder='Nombres'> 
-    </form>
+        <div class ='formDiv'>
+            <form id='registerForm'>
+                <label>Nombre</label></br>
+                <input type='text' id='name' placeholder='Nombres'> <br>
+                <label>Email</label></br>
+                <input type='text' id='email' placeholder='Email'> <br>
+                <label>Contraseña</label></br>
+                <input type='text' id='password' placeholder='Contraseña'></br>
+                <input type='checkbox' <label>Acepta términos y condiciones.</label> 
+            </form>
+        </div>
     </div>
     </div>
     `

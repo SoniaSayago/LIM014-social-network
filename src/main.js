d8841dea1d9abddeba627d1c6abcfa4c3e4ffc45
Julissa <julissa.2606@gmail.com>
2021-04-29 12:05:51 -0500

    spa - terminado

--- src/main.js
+++ src/main.js
@@ -1,27 +1,19 @@
-
-
-// Firebase configuration inicial
-const firebaseConfig = {
-    apiKey: "AIzaSyAZMA3X4IF0Nr4ihwheiEDVhaKPDYzjbrU",
-    authDomain: "socialnetwork-c9540.firebaseapp.com",
-    projectId: "socialnetwork-c9540",
-    storageBucket: "socialnetwork-c9540.appspot.com",
-    messagingSenderId: "168948973432",
-    appId: "1:168948973432:web:a5e14449e8e799a58ad28d",
-    measurementId: "G-M384C4R7W4"
-  };
-
-// Inicializar el aplicativo
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 // Este es el punto de entrada de tu aplicacion
-
 import { changeTmp } from './view-controler/router.js';
+// const firebase = require("firebase");
+// // Required for side-effects
+// require("firebase/firestore");
+
+
+
+
 
 const init = () => {
+    window.location.hash = '#/';
     changeTmp(window.location.hash);
     window.addEventListener('hashchange', () => changeTmp(window.location.hash));
+
 };
 
 window.addEventListener('load', init);
+

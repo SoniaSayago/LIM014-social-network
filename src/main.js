7c028bf0aa4cdaad92635f2f29f5bcb69a57c66a
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-04-29 09:16:24 -0500

    firebase

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+
+
+// Firebase configuration inicial
+const firebaseConfig = {
+    apiKey: "AIzaSyAZMA3X4IF0Nr4ihwheiEDVhaKPDYzjbrU",
+    authDomain: "socialnetwork-c9540.firebaseapp.com",
+    projectId: "socialnetwork-c9540",
+    storageBucket: "socialnetwork-c9540.appspot.com",
+    messagingSenderId: "168948973432",
+    appId: "1:168948973432:web:a5e14449e8e799a58ad28d",
+    measurementId: "G-M384C4R7W4"
+  };
+
+// Inicializar el aplicativo
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 // Este es el punto de entrada de tu aplicacion
 
 import { changeTmp } from './view-controler/router.js';

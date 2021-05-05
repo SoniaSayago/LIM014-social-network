d4de24e407cba6f31740b5de9744a2ed10e4fb1f
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-05-05 12:51:14 -0500

    importante merge y con daniel y sonia

--- src/view/comunidad.js
+++ src/view/comunidad.js
@@ -26,47 +26,47 @@ export default () => {
 
     const logout = document.querySelector('#logout')
     logout.addEventListener('click', e => {
-            e.preventDefault();
-            signout().then(() => {
-                    console.log('sign out');
+        e.preventDefault();
+        signout().then(() => {
+                console.log('sign out');
+            })
+            .then(() => {
+                window.location.hash = '#/login';
+                document.getElementById('header').classList.add('hide');
+            })
+
+    })
+    const fs = firebase.firestore();
+    const postList = document.querySelector('.post');
+    const setupPosts = data => {
+        if (data.lenght) {
+            let html = '';
+            data.forEach(doc => {
+                const post = doc.data()
+                console.log(post)
+                const li = ` <li class = "list-group-item" > 
+                    <h5>${post.title}<h5>
+                    <p>${post.description}</p>
+                    < /li>`;
+                html += li;
+            });
+            postList.innerHTML = html;
+        } else
+            postList.innerHTML = '<p class="text-center">Login to see Posts</p>';
+
+    }
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            fs.collection('posts')
+                .get()
+                .then((snapshot) => {
+                    setupPosts(snapshot.docs)
                 })
-                .then(() => {
-                    window.location.hash = '#/login';
-                    document.getElementById('header').classList.add('hide');
-                })
+        } else {
+            setupPosts([])
+        }
+    })
 
-        })
-        // const fs = firebase.firestore();
-        // const postList = document.querySelector('.post');
-        // const setupPosts = data => {
-        //     if (data.lenght) {
-        //         let html = '';
-        //         data.forEach(doc => {
-        //             const post = doc.data()
-        //             console.log(post)
-        //             const li = ` <li class = "list-group-item" > 
-        //             <h5>${post.title}<h5>
-        //             <p>${post.description}</p>
-        //             < /li>`;
-        //             html += li;
-        //         });
-        //         postList.innerHTML = html;
-        //     } else
-        //         postList.innerHTML = '<p class="text-center">Login to see Posts</p>';
-
-    // }
-    // auth.onAuthStateChanged(user => {
-    //     if (user) {
-    //         fs.collection('posts')
-    //             .get()
-    //             .then((snapshot) => {
-    //                 setupPosts(snapshot.docs)
-    //             })
-    //     } else {
-    //         setupPosts([])
-    //     }
-    // })
-
-    // return divElement;
+    return divElement;
 
 }

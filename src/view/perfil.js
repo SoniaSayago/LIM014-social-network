2b33889272fd4da7eb9484457dbed84b887fca36
SoniaSayago <75644975+SoniaSayago@users.noreply.github.com>
2021-05-17 17:07:21 -0500

    Perfil- maqueteado CSS & HTLM

--- src/view/perfil.js
+++ src/view/perfil.js
@@ -1,20 +1,34 @@
-import { signout, user } from '../controller/controller-auth.js';
+/* eslint-disable indent */
+import { user } from '../controller/controller-auth.js';
 
 export default () => {
-  // COD QUE PERMITE LAS VISTAS: VISTA DE REGISTRO
-  const userObject = user();
-  const viewPerfil = `
+    // COD QUE PERMITE LAS VISTAS: VISTA DE REGISTRO
+    const viewPerfil = document.createElement('div');
+    // const userId = user().uid;
+    const userObject = user();
+    const defaultValue = {
+        carrera: 'Universidad',
+        phone: 'Celular',
+        birthday: 'yyyy-MM-dd',
+        country: 'País',
+        description: 'Descripción',
+        photoCover: '../img/default-cover.gif',
+    };
+    viewPerfil.classList.add('profile-container');
+    viewPerfil.innerHTML = `
     <section class="profile-content">
     <div class="profile-information">
       <div class="cover-page">
-        <img class="cover-photo" src="${userObject.photoCover}">
+        <img class="cover-photo" src="${
+  userObject.photoCover || defaultValue.photoCover
+}">
       </div>
       <label id="select-cover" for="select-cover-photo">
         <input type="file" id="select-cover-photo" class="hide" accept="image/jpeg, image/png, image/gif">
-        <span class="edit-cover"><i class="fas fa-camera edit-photo-btn"><span class="tooltiptext">Selecciona cover photo</span></i></span>
+        <span class="edit-cover"><i class="fas fa-camera edit-photo-btn"><span class="tooltiptext">Selecciona foto de portada</span></i></span>
       </label>
       <div class="profile-photo">
-        <img class="photo" src="${userObject.photo}">
+        <img class="photo" src="${userObject.photoURL}">
       </div>
       <label id="select-profile" for="select-photo-profile">
         <input type="file" id="select-photo-profile" class="hide" accept="image/jpeg, image/png, image/gif">
@@ -22,14 +36,15 @@ export default () => {
       </label>
       <div class="user-information">
       <span class = "edit-info" id="btn-editProfile"><i class="fas fa-edit"><span class="tooltiptext">Editar información</span></i></span>
-        <h2 class="user-name">${userObject.username}</h2>
-        <h3>About me</h3>
+        <h2 class="user-name">${userObject.displayName}</h2>
+        <h3>Acerca de mi</h3>
         <div class="container-grid">
-          <div><i class="fas fa-envelope"></i><span>${userObject.email}</span></div>
-          <div><i class="fas fa-birthday-cake"></i><span>${userObject.birthday}</span></div>
-          <div><i class="fas fa-mobile-alt"></i><span>${userObject.phone}</span></div>
-          <div><i class="fas fa-map-marker-alt"></i><span>${userObject.country}</span></div>
-          <div class="item6"><i class="far fa-id-badge"></i><span>${userObject.description}</span></div>
+          <div><i class="fas fa-graduation-cap"></i></i><span>${userObject.carrera || defaultValue.carrera}</span></div>
+          <div><i class="fas fa-envelope"></i><span>${userObject.email || defaultValue.email}</span></div>
+          <div><i class="fas fa-birthday-cake"></i><span>${userObject.birthday || defaultValue.birthday}</span></div>
+          <div><i class="fas fa-mobile-alt"></i><span>${userObject.phone || defaultValue.phone}</span></div>
+          <div><i class="fas fa-map-marker-alt"></i><span>${userObject.country || defaultValue.country}</span></div>
+          <div class="item6"><i class="far fa-id-badge"></i><span>${defaultValue.description}</span></div>
         </div>
       </div>
     </div>
@@ -48,7 +63,7 @@ export default () => {
       <form class="editProfile">
         <div class="grupo">
           <label  for="usernameEdit">Nombres:</label>
-          <input type="text" id="usernameEdit" value="${userObject.username}">
+          <input type="text" id="usernameEdit" value="${userObject.displayName}">
         </div>
         <div class="grupo">
           <label  for="emailEdit">Correo:</label>
@@ -56,38 +71,27 @@ export default () => {
         </div>
         <div class="grupo">
           <label  for="phoneEdit">Celular:</label>
-          <input type="text" id="phoneEdit" value="${userObject.phone}">
+          <input type="text" id="phoneEdit" value="${userObject.phone || defaultValue.phone}">
         </div>
         <div>
           <label  for="birthdayEdit">Fecha de nacimiento:</label>
-          <input type="date" id="birthdayEdit" value="${userObject.birthday}">
+          <input type="date" id="birthdayEdit" value="${userObject.birthday || defaultValue.birthday}">
         </div>
         <div class="grupo">
           <label  for="countryEdit">País:</label>
-          <input type="text" id="countryEdit" value="${userObject.country}">
+          <input type="text" id="countryEdit" value="${userObject.country || defaultValue.country}">
         </div>
         <div class="grupo">
           <label  for="descriptionEdit">Descripción:</label>
-          <textarea id="descriptionEdit">${userObject.description}</textarea>
+          <textarea id="descriptionEdit">${userObject.description || defaultValue.description}</textarea>
         </div>
         <button type="submit" class="btn-update">Actualizar</button>
       </form>
     </section>
   </div>`;
-  const divElement = document.createElement('div');
-  divElement.innerHTML = viewPerfil;
-  document.getElementById('header').classList.remove('hide');
-};
+    const divElement = document.createElement('div');
+    divElement.innerHTML = viewPerfil;
+    document.getElementById('header').classList.remove('hide');
 
-// PRUEBA COMENTADA
-// const logout = document.querySelector('#logout')
-// logout.addEventListener('click', e => {
-//     e.preventDefault();
-//     signout().then(() => {
-//             console.log('sign out');
-//         })
-//         .then(() => {
-//             window.location.hash = '#/login';
-//             document.getElementById('header').classList.add('hide');
-//         })
-// })
+    return viewPerfil;
+};
--- src/view/post.js
+++ src/view/post.js
@@ -1,56 +1,154 @@
+/* eslint-disable indent */
 import { user } from '../controller/controller-auth.js';
-import { getDataUser } from '../controller/controller-firestore.js';
+import {
+    getDataUser,
+    deletePost,
+    updatePost,
+    updatePrivacy,
+    updateLikes,
+} from '../controller/controller-firestore.js';
 
 export const itemPost = (objPost) => {
-  const userObject = user.uid;
-  const userId = user();
-  const postElement = document.createElement('div');
-  postElement.innerHTML = `
-  <div class= 'mainpost allpost'>
-  <div class='user-post'>
-    <div class="${(userObject !== objPost.userId) ? 'hide' : 'show menu'}">
-    <i class="fas fa-ellipsis-v" "btn-menu-post"><i>
-    <ul id= "menu-post-content" class="menu-post-content">
-    <li id="edit-post-${objPost.id}" class="edit-post"><i class="fas fa-edit select"></i>Editar><li>
-    <li id="delete-post-${objPost.id}" class="delete-post"><i class="fas fa-trash-alt select"></i>Borrar><li>
-    </ul>
-  </div>
-  <img class='avatar-post' src=''/>
-  <p class='userName'></p>
-  </div>
-  <div class="content-post">
+    const userObject = user.uid;
+    const userId = user();
+    const reactionCounter = objPost.likes.length;
+    const postElement = document.createElement('div');
+    postElement.innerHTML = `
+  <div class= 'mainpost'>
+    <div class='user-post'>
+        <div class="${userObject !== objPost.userId ? 'hide' : 'show menu-post'}">
+        <i class="fas fa-ellipsis-v btn-menu-post"><i>
+        <ul id="menu-post-content" class="menu-post-content">
+          <li id="edit-post" class="edit-post"><i class="fas fa-edit select"></i>Editar><li>
+          <li id="delete-post-${objPost.id}" class="delete-post"><i class="fas fa-trash-alt select"></i>Borrar><li>
+        </ul>
+        </div>
+      <img class='avatar-post' src=''/>
+      <p class='userName'>
+        <span class = "username"></span>
+        <span class = "tooltiptext">
+          <img class="tooltipimg" src=""/>
+          <strong class="nametooltip"></strong> <br>
+          <i class="fas fa-birthday-cake"></i> &nbsp <span id="birthdayTooltip"></span><br>
+          <i class="fas fa-map-marker-alt"></i> &nbsp <span id="countryTooltip"></span>
+        </span>
+      </p>
+      
+      <select id="privacy-option" class='${
+  userObject === objPost.userId ? 'show fa' : 'hide'
+}' id='privacy-option'>
+        <option class='fa' value='public' ${
+  objPost.privacy === 'public' ? 'selected' : ''
+} title = 'Public'>&#xf57d; </option>
+        <option class='fa' value='private' ${
+  objPost.privacy === 'private' ? 'selected' : ''
+} title = 'Private'>&#xf023; </option>
+      </select>
+      <time class="time-post">${objPost.date}</time>
+    </div>
+    <div class="content-post">
       <p class="text-post">${objPost.publication}</p>
       <div class = "hide edit-text-post">
-      <textarea class="edit-text">${objPost.publication}</textarea>
-      <div class = "edit-text-btns">
-        <button type="button" class="btn-save-edit-${objPost.id}">Save</button>
-        <button type="button" class="btn-cancel-edit">Cancel</button>
+        <textarea class="edit-text">${objPost.publication}</textarea>
+        <div class = "edit-text-btns">
+          <button type="button" class="btn-save-edit-${objPost.id}">Save</button>
+          <button type="button" class="btn-cancel-edit">Cancel</button>
       </div>
-      </div>
-      <time class="time-post">${objPost.date}</time>
-  </div>
-  <img id="post-img" class="post-img" src='${objPost.urlimg}'/>
-  <div class="like-comment-container">
-     <p class="count-like" 
-    <span class = "tooltiptext"><i class="far fa-heart"></i> ${objPost.likes.length} </span>
-    </p>
-    <p id = "count-comment" class="count-comment"></p>   
-    <hr>
-    <button type="button" id="btn-like" class="btn-like-plane ${(objPost.likes.indexOf(userId) === -1) ? 'inactive-reaction' : 'active-reaction'}"><i class="far fa-heart"></i>Me gusta</button>
+    </div>
+    <img id="post-img" class="post-img" src='${objPost.urlimg}'/>
+    <div class="like-comment-container">
+      <p class=""${
+  reactionCounter === 0 ? 'hide' : 'count-like'
+}" > ${reactionCounter} reactions" 
+        <span class = "tooltiptext"><i class="far fa-heart"></i> ${objPost.likes.length} </span>
+      </p>
+      <p id = "count-comment" class="count-comment"></p>   
+      <hr>
+    <button type="button" id="btn-like" class="btn-like-plane ${objPost.likes.indexOf(userId) === -1 ? 'inactive-reaction' : 'active-reaction'}"><i class="far fa-heart"></i>Me gusta</button>
     <button type="button" id="btn-comment" class="btn-comment"><i class="fa fa-comment"></i>Comentar</button>
+    </div>
+    <section id ="container-comment" class="hide">
+      <form class="div-comment formComment">
+        <textarea class="comment" placeholder="Add a comment" required></textarea>
+        <button type="submit" class="fas fa-paper-plane"></button>
+      </form>
+      <div id = "container-AllComment"></div>
+    </section>
   </div>
-
-  </div>
+</div>
   `;
-  document.getElementById('header').classList.remove('hide');
+    document.getElementById('header').classList.remove('hide');
 
-  getDataUser(objPost.userId)
-    .then((doc) => {
-      const avatarPost = postElement.querySelector('.avatar-post');
-      const namePost = postElement.querySelector('.userName');
-      avatarPost.src = doc.data().photo;
-      namePost.textContent = doc.data().username;
+    getDataUser(objPost.userId).then((doc) => {
+        const avatarPost = postElement.querySelector('.avatar-post');
+        const namePost = postElement.querySelector('.userName');
+        const nametooltip = postElement.querySelector('.nametooltip');
+        const tooltipimg = postElement.querySelector('.tooltipimg');
+        const birthdayTooltip = postElement.querySelector('#birthdayTooltip');
+        const countryTooltip = postElement.querySelector('#countryTooltip');
+        avatarPost.src = doc.data().photo;
+        namePost.textContent = doc.data().username;
+        nametooltip.textContent = doc.data().username.toUpperCase();
+        tooltipimg.src = doc.data().photo;
+        birthdayTooltip.textContent = doc.data().birthday;
+        countryTooltip.textContent = doc.data().country;
     });
 
-  return postElement;
+    /* ---------------- Menu despegable --------------------------*/
+    const btnMenu = postElement.querySelector('.btn-menu-post');
+    btnMenu.addEventListener('click', () => {
+        postElement.querySelector('#menu-post-content').classList.toggle('show');
+    });
+    // close menu click
+    window.addEventListener('click', (e) => {
+        if (e.target !== btnMenu) {
+            postElement.querySelector('#menu-post-content').classList.remove('show');
+        }
+    });
+    //  editar y eliminar menu-------------
+    const editPost = postElement.querySelector('.edit-post');
+    const editPublication = postElement.querySelector('#edit-text');
+    const btnSaveEdit = postElement.querySelector(`.btn-save-edit-${objPost.id}`);
+    const btnCancelEdit = postElement.querySelector('.btn-cancel-edit');
+    // editar menu de post element
+    editPost.addEventListener('click', () => {
+        postElement.querySelector('.edit-text-post').classList.remove('hide');
+        postElement.querySelector('.text-post').classList.add('hide');
+    });
+    // cancelar editar post
+    btnCancelEdit.addEventListener('click', () => {
+        postElement.querySelector('.edit-text-post').classList.remove('hide');
+        postElement.querySelector('.text-post').classList.add('hide');
+        editPublication.value = objPost.publication;
+    });
+
+    // update post element
+    btnSaveEdit.addEventListener('click', () => {
+        updatePost(objPost.id, editPublication.value);
+    });
+
+    // eliminar post element
+    postElement
+        .querySelector(`#delete-post-${objPost.id}`)
+        .addEventListener('click', () => {
+            deletePost(objPost.id);
+        });
+
+    // actualizar estatus de Privacidad
+    const privacyStatus = postElement.querySelector('#privacy-option');
+    privacyStatus.addEventListener('change', () => {
+        updatePrivacy(objPost.id, privacyStatus.value);
+    });
+
+    // actualizar likes
+    const likes = postElement.querySelector('#btn-like');
+    likes.addEventListener('click', () => {
+        const result = objPost.likes.indexOf(userId);
+        if (result === -1) {
+            objPost.likes.push(userId);
+            updateLikes(objPost.id, objPost.likes);
+        }
+    });
+
+    return postElement;
 };

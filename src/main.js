6e0e44514c5ab6041ae97ee13271700ea2ab2233
MariaCristinaTC <mariacristina.tarazona@outlook.com>
2021-05-24 20:13:42 -0500

    some changes

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import { changeTmp } from './view-controler/router.js';
 
 // función que permite mostrar las vistas del usario
 const init = () => {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      changeTmp(window.location.hash);
-    } else {
-      window.location.hash = '#/login';
-    }
-  });
-  window.addEventListener('hashchange', () => changeTmp(window.location.hash));
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            changeTmp(window.location.hash);
+        } else {
+            window.location.hash = '#/login';
+        }
+    });
+    window.addEventListener('hashchange', () => changeTmp(window.location.hash));
 };
 
 // función que muestra la vista al momento de recargar
